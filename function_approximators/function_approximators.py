4a3e4e5cd987f11fb9cecd4615fc9974c5fc27fb
Antreas Tsiakkas <atsiakkas40@gmail.com>
2021-06-05 17:08:49 +0300

    finished hyperparameter tuning for linear model

--- function_approximators/function_approximators.py
+++ function_approximators/function_approximators.py
@@ -47,8 +47,8 @@ class LinearModel(FA_model):
         self.lows, self.highs, self.specs = tiling_specs
 
         # self.model = nn.Linear(input_dim, output_dim)
-        # self.model = nn.Linear(input_dim*poly_degree, output_dim)
-        self.model = nn.Linear(77, output_dim)       
+        self.model = nn.Linear(input_dim**poly_degree+input_dim, output_dim)
+        # self.model = nn.Linear(self.specs[0][0][0]**2*len(self.specs)+2, output_dim)       
 
     
     def _polynomial_features_1(self, x):
@@ -57,14 +57,15 @@ class LinearModel(FA_model):
     
     def _polynomial_features_2(self, x):
         
-        try:
+        if len(x.size()) == 2:
             f = []
             for xi in x:
                 f.append([i*j for i in xi for j in xi])
-            return torch.Tensor(f)
+            return torch.cat([x, torch.Tensor(f)], -1)
 
-        except:
-            return torch.Tensor([i*j for i in x for j in x])
+        elif len(x.size()) == 1:
+            f = [i*j for i in x for j in x]
+            return torch.cat([x, torch.Tensor(f)], -1)
 
     
     def _create_grid(self, lower, upper, bins, offsets):
@@ -104,50 +105,10 @@ class LinearModel(FA_model):
                 features.append(self._get_indices(tile_encoding, specs[0][0][0]))
 
             return torch.cat([x[:,[1,3]],torch.Tensor(features)], -1)
-
-
-    def _tiling_features_2(self, x):
           
-        tiling_coords_theta = np.array([-0.18,-0.13,-0.08,-0.03,0.07,0.12,0.17,0.22])
-        tiling_coords_pos = np.array([-3.2,-1.2,-0.7,0.3,0.8,2.8])
-
-        a = []
-
-        try:
-            for i in range(3):
-                f_tiling_theta = np.digitize(x[:,2], tiling_coords_theta+i*0.02)
-                
-                new_features_theta = np.zeros((x.shape[0],len(tiling_coords_theta)+1))
-                new_features_theta[np.arange(x.shape[0]),f_tiling_theta]=1
-                a.append(torch.Tensor(new_features_theta))
-                
-                f_tiling_pos = np.digitize(x[:,0], tiling_coords_pos+i*0.2)
-                
-                new_features_pos = np.zeros((x.shape[0],len(tiling_coords_pos)+1))
-                new_features_pos[np.arange(x.shape[0]),f_tiling_pos]=1
-                a.append(torch.Tensor(new_features_pos))
-            
-            return torch.cat([x[:,[1,3]],a[0],a[1],a[2],a[3],a[4],a[5]], -1)
-
-        except:
-            for i in range(3):
-                f_tiling_theta = np.digitize(x[2], tiling_coords_theta+i*0.02)
-                
-                new_features_theta = np.zeros(len(tiling_coords_theta)+1)
-                new_features_theta[f_tiling_theta]=1
-                a.append(torch.Tensor(new_features_theta))
-                
-                f_tiling_pos = np.digitize(x[0], tiling_coords_pos+i*0.2)
-                
-                new_features_pos = np.zeros(len(tiling_coords_pos)+1)
-                new_features_pos[f_tiling_pos]=1
-                a.append(torch.Tensor(new_features_pos))
-            
-            return torch.cat([x[[1,3]],a[0],a[1],a[2],a[3],a[4],a[5]], -1)
-            
         
     def forward(self, x): 
         
-        return self.model(self._tiling_features(x, self.lows, self.highs, self.specs))
-        # return self.model(self._polynomial_features_1(x))
+        # return self.model(self._tiling_features(x, self.lows, self.highs, self.specs))
+        return self.model(self._polynomial_features_2(x))
         # return self.model(x)
